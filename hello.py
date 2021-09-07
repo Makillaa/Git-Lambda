5c24d525ca6a0ff8b8e16947e1333d24dd81d446
Maksym Hryshchenko <35242142+Makillaa@users.noreply.github.com>
2021-09-07 22:41:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,30 @@
+import json, requests
+
+
+class Pagination:
+
+    def __init__(self, items=[]):
+        self.items = items
+        self.cursor = 0
+
+    def get_visible_items(self):
+        run = self.cursor+3
+        return self.items[self.cursor:run]
+        a = self.items.pop(self.cursor)
+        self.items.append(a)
+        if self.cursor >= len(self.items)-1:
+            self.cursor = 0
+
+
 def lambda_handler(event, context):
-    return "Hello World!"
+    response = requests("GET", "https://api.airtable.com/v0/appBULZ298jKOcjoN/MainTable?view=Grid%20view",
+                            headers={"Authorization": "Bearer keyW5hCXiZZ7WEJmf"})
+    jsonik = json.loads(response.content)
+    print(jsonik)
+    zdarov = []
+    for i in jsonik['records']:
+        zdarov.append(i['fields']['title'])
+    pig = Pagination(zdarov)
+    aim = []
+    for i in range(1):
+        return pig.get_visible_items()
